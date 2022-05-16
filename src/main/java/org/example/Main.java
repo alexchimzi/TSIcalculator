a9a013dc64bdcdf065dfbeb3d0b2625c363b9d31
alexchimzi <chilaeze@gmail.com>
2022-05-16 17:33:57 +0100

    added methods to get user inputs and code cleaning

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,36 +5,52 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        double leng,width,area;
-        ArrayList<String>  myArray = new ArrayList<>();
+    public static double getUserinput(double x){
         Scanner myScanner  = new Scanner(System.in);
+        x = myScanner.nextDouble();
+        return x;
+    }
+    public static int getUserinput(int x){
+        Scanner myScanner  = new Scanner(System.in);
+        x = myScanner.nextInt();
+        return x;
+    }
+    public static boolean getUserinput(boolean x){
+        Scanner myScanner  = new Scanner(System.in);
+        x = myScanner.nextBoolean();
+        return x;
+    }
+    public static void main(String[] args) {
+        double leng= 0,width= 0,len = 0,wid = 0,bucket = 0, z = 0;
+        boolean a = false;
+        int Loop= 0;
+        ArrayList<String>  myArray = new ArrayList<>();
+
 
         System.out.println(" How many walls would you like to calculated? ");
-        int Loop = myScanner.nextInt();
+        Loop = getUserinput(Loop);
 
-        for(int q =0;q < Loop;q++ ) {
-            System.out.println(" Enter height of wall ");
-            leng = myScanner.nextDouble();
-            System.out.println(" Enter width of wall ");
-            width = myScanner.nextDouble();
-            area = leng * width;
+        for(int q =1;q <=Loop;q++ ) {
+            System.out.println(" Enter height of Wall "+ q);
+            leng = getUserinput(leng);
+            System.out.println(" Enter width of Wall "+q);
+            width = getUserinput(width);
+            double area = leng * width;
 
             System.out.println(" Is there a Socket or window True or False ");
-            boolean a = myScanner.nextBoolean();
+            a = getUserinput(a);
             if (a) {
-                System.out.println(" Enter height of wall ");
-                double len = myScanner.nextDouble();
-                System.out.println(" Enter width of wall ");
-                double wid = myScanner.nextDouble();
+                System.out.println(" Enter height of Socket or Window "+q);
+                len = getUserinput(len);
+                System.out.println(" Enter width of Socket or Window "+q);
+                len = getUserinput(wid);
                 area -= (len * wid);
             }
-            System.out.println(" How many sqrt feet can your paint bucket cover");
-            double bucket = myScanner.nextDouble();
+            System.out.println(" what is the coverage of your paint Bucket");
+            bucket = getUserinput(bucket);
 
             System.out.println(" How many liters in a bucket of paint ");
-            double z = myScanner.nextDouble();
-
+            z = getUserinput(z);
             myArray.add(" You will need "+ (area / bucket)+" liters of paint  to  cover the wall of "+ area + " You will need "+ (area/bucket)/z + " buckets \n");
 
         }
